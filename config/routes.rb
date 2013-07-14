bfa958672f695e4c5adfae9df1c962696f36f563
Nathaniel Jones <n@nthj.me>
2013-07-14 17:52:57 -0500

    Mount at console

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Detective::Application.routes.draw do
   devise_for :admins
   devise_for :users
 
-  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+  mount RailsAdmin::Engine => '/console', :as => 'rails_admin'
 
 
   root :to => "home#landing"
