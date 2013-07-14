d4d2f4384b6580111e41532d2b8ce60c224f9590
Nathaniel Jones <n@nthj.me>
2013-07-14 16:46:23 -0500

    Update title

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Detective::Application.routes.draw do
-  
   devise_for :admins
-  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
-  # devise_for :admins
   devise_for :users
-  
+
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
 
   root :to => "home#landing"
 end
