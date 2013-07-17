1ab5bcca8f512722d4ee1c1a579929f5c5cb8a50
jonahaaron <jh@thirdprestige.com>
2013-07-16 20:17:20 -0500

    Fixed private_eye sidebar, added detective.jpg

--- app/assets/javascripts/affix.js
+++ app/assets/javascripts/affix.js
@@ -1,9 +1,5 @@
-$(window).scroll(function(){
-  // Has the visitor scrolled down past the headmast?
-  // If the headmast is 100 pixels tall, and
-  // the visitor has scrolled down 115 pixels, then this will return true
-  //
-  $('#contact').css({
-    top: $(window).scrollTop() + 'px'
-  });
-});
+// $(window).scroll(function(){
+//   $('#private_eye').css({
+//     top: $(window).scrollTop() + 'px'
+//   });
+// });
